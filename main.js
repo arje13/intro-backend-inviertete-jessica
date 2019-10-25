14132a2594bde08ee06211ec17be3ae9cd75285e
unknown <vargasgjessicae@gmail.com>
2019-10-25 18:26:54 -0500

    Se incluyo urlencoded

--- main.js
+++ main.js
@@ -4,7 +4,25 @@ const express = require ('express')
 
 const colors = require ('colors')
 
+const bodyParser = require ('body-parser')
+
+//Aplicacion
 const app = express()
+
+//Permiso a usuario
+app.use((req,res, next)=> {
+    res.header ('Acess-Control-Allow-Origin', '*')
+
+    res.header ('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth-token')
+
+    res.header ('Acess-Control-Expose-Headers', 'x-auth-token')
+
+    next()
+})
+
+app.use (bodyParser.urlencoded({
+    extended: true
+}))
 
 var port = 8080
 
@@ -15,6 +33,11 @@ app.get('/', () => {
 })
 app.get ('/jessica', () => {
     console.log('Mi nombre es Jessica')
+})
+app.post('/nombre', (req, res)=>{
+    var name = req.body.name
+
+    res.send('Hola ' + name + ' un gusto conocerte')
 })
 app.get ('/backend', (req, res) => {
     res.send('Entraste a /')
